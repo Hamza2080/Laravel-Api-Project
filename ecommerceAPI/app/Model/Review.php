a877c2a7b066e88ccd77261e1a9518609ecd7609
Hamza <hamza.bsse2080@iiu.edu.pk>
2018-02-24 22:30:11 +0500

    creating relations b/w apis

--- ecommerceAPI/app/Model/Review.php
+++ ecommerceAPI/app/Model/Review.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Model;
+use App\Model\Product;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // creating relations
+    public function product () {
+        return $this->belongsTo(Product::class);
+    }
 }
