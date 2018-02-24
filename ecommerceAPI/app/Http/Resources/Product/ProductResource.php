fc178666cf3061966c5d9dda125b5135855fd5f1
Hamza <hamza.bsse2080@iiu.edu.pk>
2018-02-24 23:20:58 +0500

    modified product details(add links in detail)

--- ecommerceAPI/app/Http/Resources/Product/ProductResource.php
+++ ecommerceAPI/app/Http/Resources/Product/ProductResource.php
@@ -16,9 +16,15 @@ class ProductResource extends JsonResource
     {
         return [
             'name' => $this->name,
+            'stock' => $this->stock == 0 ? 'Not avilable in stock yet' : $this->stock,
             'description' => $this->detail,
             'price' => $this->price,
-            'discount' => $this->discount
+            'discount' => $this->discount,
+            'totalPrice' =>  ($this->price - ($this->discount/100)* $this->price),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No Ratting Yet',
+            'href' => [
+                'reviews' => route('reviews.index' , $this->id)
+            ]
         ];
     }
 }
